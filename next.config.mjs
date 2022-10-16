81dce358d8992f825f08ef1876d48b40f4b44fe8
Thinh Tran <thinhrom11@gmail.com>
2022-10-16 14:33:47 +0700

    chore: migrate trpc to v10 and change the schema slightly

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import { env } from "./src/env/server.mjs"
 
 /**
  * Don't be scared of the generics here.
@@ -17,7 +16,7 @@ export default defineNextConfig({
   swcMinify: true,
   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["vi"],
+    defaultLocale: "vi",
   },
 });
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,16 +1,13 @@
 // src/pages/api/trpc/[trpc].ts
 import { createNextApiHandler } from "@trpc/server/adapters/next"
 import { env } from "../../../env/server.mjs"
-import { appRouter } from "../../../server/router"
-import { createContext } from "../../../server/router/context"
+import { createContext } from "../../../server/trpc/context"
+import { appRouter } from "../../../server/trpc/router/_app"
 
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
   createContext,
-  batching: {
-    enabled: true,
-  },
   onError:
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
