1fa389cb51a11905215912bad02de891e2756dd7
johnhoangsoftware <johnhoangsoftware@gmail.com>
2022-12-20 01:36:27 +0700

    chore: fix update-profile page '

--- src/pages/update-profile.tsx
+++ src/pages/update-profile.tsx
@@ -81,14 +81,14 @@ export default function UpdateProfile() {
                 <div className="w-full px-3 sm:w-1/2">
                   <div className="mb-5">
                     <label
-                      htmlFor="date"
+                      htmlFor="birthday"
                       className="mb-3 block text-base font-medium text-[#07074D]">
-                      Date
+                      Birthday
                     </label>
                     <input
                       type="date"
-                      name="date"
-                      id="date"
+                      name="birthday"
+                      id="birthday"
                       className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                     />
                   </div>
